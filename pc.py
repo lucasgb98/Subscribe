bcd800bc2ff3abc10189573a4c1b59bdeb1482ca
lucasgb98 <88604749+lucasgb98@users.noreply.github.com>
2021-10-23 02:24:18 -0300

    Update pc.py

--- pc.py
+++ pc.py
@@ -432,7 +432,7 @@ def loadConfig():
             + Fore.RESET
             + getString("register_warning"))
 
-        username = "Nitinkumar"
+        username = "lucasgb98"
 
         if xxhash_enabled:
             print(
